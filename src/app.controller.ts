6d8f1203f446940433db449daae836f8acb90830
SirSerje <sir.serje@gmail.com>
2019-11-18 20:00:04 +0200

    add nest, add readme, update docker container

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,33 @@
 
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import {Controller, Get, Request, Post, UseGuards, Body} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth/auth.service';
+import {AppService} from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,
+              private readonly appService: AppService) {}
+  //
+  // @Get('hello')
+  // doSome(@Request() req:Request) {
+  //
+  //   this.appService.getHello(req.body);
+  //   this.appService.getHello(req.headers);
+  //   return 'ssss'
+  // }
+
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
+
+  // @Get('add')
+  // async doAny(@Request() req: Request) {
+  //   return this.authService.createNewUser({email:'asd'})
+  // }
 
   @UseGuards(AuthGuard('jwt'))
   @Get('profile')
