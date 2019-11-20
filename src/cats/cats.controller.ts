10281ecfa28c69f796a4d5017a67c4e4ceee8340
SirSerje <sir.serje@gmail.com>
2019-11-20 22:44:41 +0200

    working on simple CRUD

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,32 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {Controller, Get, Post, Body, Delete, Put, Param} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+
+@Controller('cat')
+export class CatController {
+  constructor(private readonly catsService: CatsService) {}
+  //C
+  @Post()
+  async createCat(@Body() createCatDto: CreateCatDto) {
+    await this.catsService.create(createCatDto);
+  }
+  //R
+  @Get('/:id')
+  async readCat(@Param('id') catId) {
+    return `GET request ${catId}`
+  }
+  //U
+  @Put('/:id')
+  async updateCat(@Param('id') catId) {
+    return `PUT request ${catId}`
+  }
+  //D
+  @Delete('/:id')
+  async deleteCat(@Param('id') catId) {
+    return `DEL request ${catId}`
+  }
+}
 
 @Controller('cats')
 export class CatsController {
